139c12453a7a92f6fda0d7420f0b7ce83a6b8fc6
Fabian Ommer <fabian.ommer@gmail.com>
2024-01-10 23:35:16 +0100

    Version bump

--- pixels-dndbeyond.user.js
+++ pixels-dndbeyond.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixels DnD Beyond
 // @namespace    http://tampermonkey.net/
-// @version      0.3.7
+// @version      0.3.8
 // @description  Use Pixel Dice on DnD Beyond
 // @author       carrierfry
 // @match        https://www.dndbeyond.com/characters/*
