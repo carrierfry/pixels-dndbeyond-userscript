5282ebdb44f4ade9c5360c19e30095d3b71e1d1d
Fabian Ommer <fabian.ommer@gmail.com>
2024-01-14 01:56:43 +0100

    It was suggested that crit shouldn't be automatically applied in case people play with different crit rules

--- pixels-dndbeyond.user.js
+++ pixels-dndbeyond.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixels DnD Beyond
 // @namespace    http://tampermonkey.net/
-// @version      0.4.3.3
+// @version      0.4.3.4
 // @description  Use Pixel Dice on DnD Beyond
 // @author       carrierfry
 // @match        https://www.dndbeyond.com/characters/*
@@ -1209,10 +1209,10 @@ function determineRollType(rollButton) {
                 }
             };
         }
-        if (nextDmgRollIsCrit) {
-            crit = true;
-            nextDmgRollIsCrit = false;
-        }
+        // if (nextDmgRollIsCrit) {
+        //     crit = true;
+        //     nextDmgRollIsCrit = false;
+        // }
     }
     return { adv, dis, crit };
 }
