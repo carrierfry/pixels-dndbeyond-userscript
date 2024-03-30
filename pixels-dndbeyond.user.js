bb5342872367ca8168457a6d808c0723144c7a22
Fabian Ommer <fabian.ommer@gmail.com>
2024-03-30 16:21:48 +0100

    Fixed a bug with the game log and pixel mode

--- pixels-dndbeyond.user.js
+++ pixels-dndbeyond.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixels DnD Beyond
 // @namespace    http://tampermonkey.net/
-// @version      0.9.2.1
+// @version      0.9.2.2
 // @description  Use Pixel Dice on DnD Beyond
 // @author       carrierfry
 // @match        https://www.dndbeyond.com/characters/*
@@ -2952,7 +2952,7 @@ function checkIfDieTypeIsConnected(dieType) {
 }
 
 function checkIfDiceButtonCanBeSwappedAgain(currentButton, newButton) {
-    let element = document.querySelector(".MuiPaper-root");
+    let element = document.querySelector(".MuiPaper-root > div");
     if (element !== null) {
         lastRightClickedButton = currentButton;
         currentlySwapped = true;
