70bf065b3326d445ee3273f99b6624a3b580befc
Fabian Ommer <fabian.ommer@gmail.com>
2024-10-29 21:25:23 +0100

    Fix for dndd beyond renaming stuff and custom modifiers now range from -10 to 10

--- pixels-dndbeyond.user.js
+++ pixels-dndbeyond.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixels DnD Beyond
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1.0
+// @version      1.0.1.1
 // @description  Use Pixel Dice on DnD Beyond
 // @author       carrierfry
 // @license      MIT
@@ -1501,8 +1501,6 @@ function addPixelsLogoButton() {
 
         // create a link
         let link = document.createElement("a");
-        // link.className = "mm-nav-item__label mm-nav-item__label--link";
-        link.className = "_link_18gwk_21";
         if (isMobileView || isTabletView || (isEncounterBuilder && document.querySelector(".menu-button").checkVisibility())) {
             link.className = "nav-list__item__label";
         }
@@ -1518,9 +1516,11 @@ function addPixelsLogoButton() {
         };
         button.appendChild(link);
         // find the last mm-nav-item and insert after it
-        let lastNavItem = document.querySelector("._content_18gwk_8").children;
+        let lastNavItem = document.querySelector("menu[class^='_content_'").children;
+        link.className = "_link_" + document.querySelector("menu[class^='_content_'").className.substring(9).slice(0, -2) + "_21";
         if (isMobileView || isTabletView || (isEncounterBuilder && document.querySelector(".menu-button").checkVisibility())) {
             lastNavItem = document.querySelectorAll(".nav-list__item");
+            link.className = "";
         }
         lastNavItem = lastNavItem[lastNavItem.length - 1];
         lastNavItem.parentNode.insertBefore(button, lastNavItem.nextSibling);
@@ -1723,7 +1723,7 @@ function rollDice(realDieType, value) {
             }
             if (enableCustomModifiers) {
                 modifier += parseInt(document.getElementById("customModifierSelect").value);
-                document.getElementById("customModifierSelect").selectedIndex = 5;
+                document.getElementById("customModifierSelect").selectedIndex = 10;
             }
             if (Object.keys(currentlyExpectedRoll).length > 0) {
                 initJson = buildInitialJson(dieType, modifier, amount, getRollKind(), currentlyExpectedRoll.rollType, currentlyExpectedRoll.rollName, currentlyExpectedRoll.target, currentlyExpectedRoll.scope);
@@ -2425,7 +2425,7 @@ function addOrRemoveCustomModifiersDOM() {
         let buttonsDiv = document.querySelector(".pixels-info-box__content__buttons_overview");
 
         let customModifierDiv = document.createElement("div");
-        customModifierDiv.innerHTML = "<p id='customModifierText' class='pixels-info-box__content__custom_modifier_text'>Custom Modifier:</p> <select id='customModifierSelect' class='pixels-info-box__content__custom_modifier_select'><option value='-5'>-5</option><option value='-4'>-4</option><option value='-3'>-3</option><option value='-2'>-2</option><option value='-1'>-1</option><option value='0' selected>0</option><option value='1'>+1</option><option value='2'>+2</option><option value='3'>+3</option><option value='4'>+4</option><option value='5'>+5</option></select>";
+        customModifierDiv.innerHTML = "<p id='customModifierText' class='pixels-info-box__content__custom_modifier_text'>Custom Modifier:</p> <select id='customModifierSelect' class='pixels-info-box__content__custom_modifier_select'><option value='-10'>-10</option><option value='-9'>-9</option><option value='-8'>-8</option><option value='-7'>-7</option><option value='-6'>-6</option><option value='-5'>-5</option><option value='-4'>-4</option><option value='-3'>-3</option><option value='-2'>-2</option><option value='-1'>-1</option><option value='0' selected>0</option><option value='1'>+1</option><option value='2'>+2</option><option value='3'>+3</option><option value='4'>+4</option><option value='5'>+5</option><option value='6'>+6</option><option value='7'>+7</option><option value='8'>+8</option><option value='9'>+9</option><option value='10'>+10</option></select>";
         customModifierDiv.classList.add("pixels-info-box__content__custom_modifier");
 
         buttonsDiv.insertAdjacentElement("beforebegin", customModifierDiv);
