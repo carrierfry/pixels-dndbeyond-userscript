9e183c5d8b962f53e857b72428632085e68f0f5c
carrierfry <fabian.ommer@gmail.com>
2026-08-10 00:29:42 +0200

    Bump version to 1.0.5.3

--- pixels-dndbeyond.user.js
+++ pixels-dndbeyond.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixels DnD Beyond
 // @namespace    http://tampermonkey.net/
-// @version      1.0.5.2
+// @version      1.0.5.3
 // @description  Use Pixel Dice on DnD Beyond
 // @author       carrierfry
 // @license      MIT
